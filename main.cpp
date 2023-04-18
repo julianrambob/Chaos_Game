0b71719a84dc5d7c32c5f85a8a366f386d0731e3
julianrambob <julianrambob@gmail.com>
2023-04-18 15:42:15 -0700

    push with click events

--- main.cpp
+++ main.cpp
@@ -7,11 +7,15 @@ using namespace sf;
 using namespace std;
 int main()
 {
-	sf::View view(sf::FloatRect(0.f, 0.f, 1920.f, 1080.f));
+	sf::View view(sf::FloatRect(0.f, 0.f, 800.f, 600.f));
 	// Create a video mode object
-	VideoMode vm(1920, 1080);
+	VideoMode vm(800, 600);
 	// Create and open a window for the game
-	RenderWindow window(vm, "Chaos Game!!", Style::Fullscreen);
+	RenderWindow window(vm, "Chaos Game", Style::Default);
+
+	vector<Vector2f> vertices;
+	vector<Vector2f> points;
+
 	while (window.isOpen())
 	{
 		/*
@@ -22,14 +26,28 @@ int main()
 		Event event;
 		while (window.pollEvent(event))
 		{
-			/*if (event.type == Event::KeyReleased && !paused)
+			if (event.type == Event::Closed)
 			{
-			}*/
+				window.close();
+			}
+
+			//mouse click event
+			if (event.type == sf::Event::MouseButtonPressed)
+			{
+				if (event.mouseButton.button == sf::Mouse::Left)
+				{
+					std::cout << "the right button was pressed" << std::endl;
+					std::cout << "mouse x: " << event.mouseButton.x << std::endl;
+					std::cout << "mouse y: " << event.mouseButton.y << std::endl;
+					vertices.push_back({(float) event.mouseButton.x, (float) event.mouseButton.y });
+				}
+			}
 		}
+
 		/*****************************************
 			Update the scene
 		*****************************************/
-		//do stuff
+		//generate points
 		/*
 		****************************************
 		Draw the scene
@@ -39,7 +57,15 @@ int main()
 		// Clear everything from the last run frame
 		window.clear();
 		// Draw our game scene here
-		//window.draw(
+		RectangleShape r{ Vector2f{ 10, 10} }; //width, height. Center uninitialized
+		r.setFillColor(Color::Yellow);
+		for (size_t i = 0; i < vertices.size(); i++)
+		{
+
+			r.setPosition(Vector2f{ vertices.at(i).x, vertices.at(i).y });
+			window.draw(r);
+		}
+
 		// Show everything we just drew
 		window.display();
 	}
