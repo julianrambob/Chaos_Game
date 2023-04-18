4ece0d2f3c127acb9a193cb1c6da7482dc159102
julianrambob <julianrambob@gmail.com>
2023-04-18 16:59:00 -0700

    mid point starter code commented out

--- main.cpp
+++ main.cpp
@@ -52,6 +52,18 @@ int main()
 					vertices.push_back({(float) event.mouseButton.x, (float) event.mouseButton.y });
 				}
 			}
+
+			if (vertices.size() == 3)
+			{
+				if (event.type == sf::Event::MouseButtonPressed)
+				{
+					if (event.mouseButton.button == sf::Mouse::Left)
+					{
+						std::cout << "the right button was pressed" << std::endl;
+						points.push_back({ (float)event.mouseButton.x, (float)event.mouseButton.y });
+					}
+				}
+			}
 		}
 
 		/*****************************************
@@ -71,13 +83,47 @@ int main()
 		// Draw our game scene here
 		RectangleShape r{ Vector2f{ 10, 10} }; //width, height. Center uninitialized
 		r.setFillColor(Color::Yellow);
+			
 		for (size_t i = 0; i < vertices.size(); i++)
-		{
-
-			r.setPosition(Vector2f{ vertices.at(i).x, vertices.at(i).y });
+		{	
+			r.setPosition(Vector2f{ vertices.at(i).x, vertices.at(i).y });	
 			window.draw(r);
 		}
 
+
+		//something like this maybe?
+		//if (vertices.size() == 3)
+		//{
+
+		//	RectangleShape p{ Vector2f { 1, 1} };
+		//	p.setFillColor(Color::Cyan);
+
+		//	for (size_t i = 0; i < points.size(); i++)
+		//	{
+		//		int random = rand() % 3 + 1;
+		//		if (random == 1)
+		//		{
+		//			Vector2f temp((vertices.at(0).x + vertices.at(1).x) / 2, (vertices.at(0).y + vertices.at(1).y) / 2);
+		//			p.setPosition(temp);
+		//			points.push_back(temp);
+		//			window.draw(p);
+		//		}
+		//		else if (random == 2)
+		//		{
+		//			Vector2f temp((vertices.at(1).x + vertices.at(2).x) / 2, (vertices.at(1).y + vertices.at(2).y) / 2);
+		//			p.setPosition(temp);
+		//			points.push_back(temp);
+		//			window.draw(p);
+		//		}
+		//		else if (random == 3)
+		//		{
+		//			Vector2f temp((vertices.at(0).x + vertices.at(2).x) / 2, (vertices.at(0).y + vertices.at(2).y) / 2);
+		//			p.setPosition(temp);
+		//			points.push_back(temp);
+		//			window.draw(p);
+		//		}
+		//	}
+		//}
 		window.draw(instructions);
 
 		// Show everything we just drew
