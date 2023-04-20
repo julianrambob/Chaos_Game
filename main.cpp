0100f21c6ac4fddec7980a6c7d17e491f049d5ae
julianrambob <julianrambob@gmail.com>
2023-04-19 18:06:50 -0700

    Final code

--- main.cpp
+++ main.cpp
@@ -7,14 +7,20 @@ using namespace sf;
 using namespace std;
 int main()
 {
-	sf::View view(sf::FloatRect(0.f, 0.f, 800.f, 600.f));
+	float width = sf::VideoMode::getDesktopMode().width;
+	float height = sf::VideoMode::getDesktopMode().height;
+	View mainView(FloatRect(0.0f, 0.0f, width, height));
+	//width = 800; height = 600;
 	// Create a video mode object
-	VideoMode vm(800, 600);
+	VideoMode vm(width, height);
 	// Create and open a window for the game
 	RenderWindow window(vm, "Chaos Game", Style::Default);
 
 	vector<Vector2f> vertices;
 	vector<Vector2f> points;
+
+	bool chaos = false;
+	int point_counter = 0;
 
 	Font f;
 	f.loadFromFile("ApeMount-WyPM9.ttf");
@@ -46,22 +52,24 @@ int main()
 			{
 				if (event.mouseButton.button == sf::Mouse::Left)
 				{
-					std::cout << "the right button was pressed" << std::endl;
-					std::cout << "mouse x: " << event.mouseButton.x << std::endl;
-					std::cout << "mouse y: " << event.mouseButton.y << std::endl;
-					vertices.push_back({(float) event.mouseButton.x, (float) event.mouseButton.y });
+						std::cout << "the left button was pressed" << std::endl;
+						std::cout << "mouse x: " << event.mouseButton.x << std::endl;
+						std::cout << "mouse y: " << event.mouseButton.y << std::endl;
+						vertices.push_back({ (float)event.mouseButton.x, (float)event.mouseButton.y });
 				}
 			}
 
-			if (vertices.size() == 3)
+			if (event.type == sf::Event::MouseButtonPressed)
 			{
-				if (event.type == sf::Event::MouseButtonPressed)
+				if (event.mouseButton.button == sf::Mouse::Right)
 				{
-					if (event.mouseButton.button == sf::Mouse::Left)
+					if (vertices.size() >= 3)
 					{
 						std::cout << "the right button was pressed" << std::endl;
 						points.push_back({ (float)event.mouseButton.x, (float)event.mouseButton.y });
+						chaos = true;
 					}
+
 				}
 			}
 		}
@@ -71,59 +79,45 @@ int main()
 		*****************************************/
 		//generate points
 
-		instructions.setString("Click Three points, then click once to start the Chaos");
+		instructions.setString("Left click the vertices of your shape, then right click to start the Chaos");
+
+		if (chaos)
+		{
+			int random = rand() % vertices.size();
+			Vector2f next_p(static_cast<float>((vertices.at(random).x + points.at(point_counter).x) / 2), static_cast<float>((vertices.at(random).y + points.at(point_counter).y )/ 2));
+			point_counter++;
+			points.push_back(next_p);
+
+		}
+
 		/*
 		****************************************
 		Draw the scene
 		****************************************
 		*/
-		window.setView(view);
+		window.setView(mainView);
 		// Clear everything from the last run frame
 		window.clear();
 		// Draw our game scene here
-		RectangleShape r{ Vector2f{ 10, 10} }; //width, height. Center uninitialized
-		r.setFillColor(Color::Yellow);
-			
+		RectangleShape r{ Vector2f{ 5, 5} }; //width, height. Center uninitialized
+		r.setFillColor(Color::Cyan);
+
 		for (size_t i = 0; i < vertices.size(); i++)
-		{	
-			r.setPosition(Vector2f{ vertices.at(i).x, vertices.at(i).y });	
+		{
+			r.setPosition(Vector2f{ vertices.at(i).x, vertices.at(i).y });
 			window.draw(r);
 		}
 
+		RectangleShape p{ Vector2f{1, 1} };
+		p.setFillColor(Color::Magenta);
 
-		//something like this maybe?
-		//if (vertices.size() == 3)
-		//{
+		for (size_t i = 0; i < points.size(); i++)
+		{
+			p.setPosition(Vector2f{ points.at(i).x, points.at(i).y });
+			window.draw(p);
+		}
 
-		//	RectangleShape p{ Vector2f { 1, 1} };
-		//	p.setFillColor(Color::Cyan);
-
-		//	for (size_t i = 0; i < points.size(); i++)
-		//	{
-		//		int random = rand() % 3 + 1;
-		//		if (random == 1)
-		//		{
-		//			Vector2f temp((vertices.at(0).x + vertices.at(1).x) / 2, (vertices.at(0).y + vertices.at(1).y) / 2);
-		//			p.setPosition(temp);
-		//			points.push_back(temp);
-		//			window.draw(p);
-		//		}
-		//		else if (random == 2)
-		//		{
-		//			Vector2f temp((vertices.at(1).x + vertices.at(2).x) / 2, (vertices.at(1).y + vertices.at(2).y) / 2);
-		//			p.setPosition(temp);
-		//			points.push_back(temp);
-		//			window.draw(p);
-		//		}
-		//		else if (random == 3)
-		//		{
-		//			Vector2f temp((vertices.at(0).x + vertices.at(2).x) / 2, (vertices.at(0).y + vertices.at(2).y) / 2);
-		//			p.setPosition(temp);
-		//			points.push_back(temp);
-		//			window.draw(p);
-		//		}
-		//	}
-		//}
+	
 		window.draw(instructions);
 
 		// Show everything we just drew
@@ -132,3 +126,4 @@ int main()
 
 	return 0;
 }
+	
